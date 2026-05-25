41ba2959eb5951b0f72e9ede02ec72cadda8624f
vincent12123 <mrsukardi@gmail.com>
2026-05-25 18:35:24 +0700

    feat: add RolesAndAdminSeeder to DatabaseSeeder class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
-{
-    $this->call([
-        RolesAndAdminSeeder::class,
-    ]);
-}
+    {
+        $this->call([
+            RolesAndAdminSeeder::class,
+        ]);
+    }
 }
